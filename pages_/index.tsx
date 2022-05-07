1e894ccc576e839c7d27c3c6459e89690a41a998
Matthias Reichmann <matreich@uni-bremen.de>
2022-05-07 17:58:03 +0200

    feat: add components sections to page

--- pages_/index.tsx
+++ pages_/index.tsx
@@ -65,6 +65,87 @@ const IndexPage = () => {
                 </h2>
             </section>
             <section className="mb-64 md:mb-96">
+                <h1 className="mb-8 md:mb-16 max-w-4xl xl:max-w-7xl">{ t`common:components.title` }</h1>
+
+                <h2 className="font-medium text-xl md:text-5xl leading-snug md:leading-snug max-w-5xl">{ t`common:components.hotels.title` }</h2>
+                <div className="mt-4">{ t`common:components.hotels.description` }</div>
+                <div className="mt-4">{ t`common:components.hotels.description2` }</div>
+                <div className="grid grid-cols-3 grid-flow-row gap-4 mt-8">
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.hotels.authorization.title` }</h3>
+
+                        <p className="font-bold mt-4">dcaf-rs</p>
+                        { t`common:components.hotels.authorization.dcaf-rs` }
+
+                        <p className="font-bold mt-4">Canephora</p>
+                        { t`common:components.hotels.authorization.canephora` }
+
+                        <p className="font-bold mt-4">tinydtls-rs</p>
+                        { t`common:components.hotels.authorization.tinydtls-rs` }
+
+                        <p className="font-bold mt-4">libcoap-rs</p>
+                        { t`common:components.hotels.authorization.libcoap-rs` }
+                    </div>
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.hotels.client.title` }</h3>
+                        { t`common:components.hotels.client.description` }
+
+                        <p className="font-bold mt-4">dart_dcaf</p>
+                        { t`common:components.hotels.client.dart_dcaf` }
+
+                        <p className="font-bold mt-4">dart_tinydtls</p>
+                        { t`common:components.hotels.client.dart_tinydtls` }
+
+                        <p className="font-bold mt-4">dart_wot</p>
+                        { t`common:components.hotels.client.dart_wot` }
+                    </div>
+                </div>
+
+                <h2 className="mt-16 font-medium text-xl md:text-5xl leading-snug md:leading-snug max-w-5xl">{ t`common:components.reverse.title` }</h2>
+                <div className="mt-4">{ t`common:components.reverse.description` }</div>
+                <div className="grid grid-cols-3 grid-flow-row gap-4 mt-8">
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.reverse.mudsights.title` }</h3>
+                        { t`common:components.reverse.mudsights.description` }
+
+                        <p className="font-bold mt-4">Toolbox</p>
+                        { t`common:components.reverse.mudsights.toolbox` }
+                    </div>
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.reverse.shai.title` }</h3>
+                        { t`common:components.reverse.shai.description` }
+
+                        <p className="font-bold mt-4">shai-netimg</p>
+                        { t`common:components.reverse.shai.netimg` }
+                    </div>
+                </div>
+
+                <h2 className="mt-16 font-medium text-xl md:text-5xl leading-snug md:leading-snug max-w-5xl">{ t`common:components.subnet.title` }</h2>
+                <div className="mt-4">{ t`common:components.subnet.description` }</div>
+                <div className="grid grid-cols-3 grid-flow-row gap-4 mt-8">
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.subnet.controller.title` }</h3>
+
+                        <ul className="list-disc ml-8 mt-4">
+                            <li className="mb-2">{ t`common:components.subnet.controller.description` }</li>
+                            <li>{ t`common:components.subnet.controller.description2` }</li>
+                        </ul>
+                    </div>
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.subnet.enforcer.title` }</h3>
+
+                        <ul className="list-disc ml-8 mt-4">
+                            <li className="mb-2">{ t`common:components.subnet.enforcer.description` }</li>
+                            <li>{ t`common:components.subnet.enforcer.description2` }</li>
+                        </ul>
+                    </div>
+                    <div className="border border-gray-600 p-4">
+                        <h3 className="font-medium text-xl md:text-2xl">{ t`common:components.subnet.muddles.title` }</h3>
+                        { t`common:components.subnet.muddles.description` }
+                    </div>
+                </div>
+            </section>
+            <section className="mb-64 md:mb-96">
                 <h1 className="mb-8 md:mb-16 max-w-4xl xl:max-w-7xl">{ t`common:cta.title` }</h1>
                 <a href="https://www.informatik.uni-bremen.de/projekttag/2021/" className="mr-6">
                     <Button size={ ButtonSize.XL }>{ t`common:cta.button-label.bachelor` }</Button>
